c335dcaf145dfbf57671bb4f6e5dbf8c460d6a01
divrawat <divrawat2001@gmail>
2025-03-24 20:19:11 +0530

    sg0rg5

--- controllers/user.js
+++ controllers/user.js
@@ -93,7 +93,7 @@ export const verifyOTP = async (req, res) => {
 export const fetchUser = async (req, res) => {
     try {
         const { userId } = req.params;
-        const user = await User.findById({ userId });
+        const user = await User.findById(userId);
         if (!user) { return res.status(404).json({ message: "User not found" }); }
         res.status(200).json({ message: "User Fetched Successfully", user });
     }
